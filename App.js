1b5cbc9ff2b9f433ac6e9c67a300bcddc6dcb13f
sean <seanpetermcelroy@gmail.com>
2021-05-08 16:38:37 +0100

    Added card component, looks nice

--- App.js
+++ App.js
@@ -7,22 +7,22 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImage from './app/screens/ViewImage'
 import AppText from './app/assets/components/AppText'
 import ButtonComponent from './app/assets/components/ButtonComponent'
-
+import Card from './app/assets/components/Card';
 
 export default function App() {
   return (
   // <WelcomeScreen/>
   // <ViewImage/>
-    <WelcomeScreen style={styles.container}>
-  
-    </WelcomeScreen>
+  <View style={styles.container}>
+    <Card title="Red Jacket" subTitle="$200" image={require("./app/assets/chair.jpg")} />
+  </View>
     )
 }
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    backgroundColor:'#f8f4f4',
+    padding: 20,
+    paddingTop: 100
+  },
 })
